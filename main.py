cca87e3ac14630c96e3169ca430a24404862b6e8
ศิรพล ศรีอวยพรชัย <s.sirapol@hotmail.com>
2021-07-03 01:41:21 +0700

    fix windows and textbox

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import tkinter as tk
 
 windows = tk.Tk()
 
+windows.geometry("500x500")               #   set windows size
+windows.resizable(False,False)          #   fix size
+
+T = tk.Text(windows, height=1, width=4)
+T.pack()
+T.insert(tk.END, "0000")
+
 windows.mainloop()
